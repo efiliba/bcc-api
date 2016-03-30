cf445692ff1634b35045b2e53fc4c51ff3de6718
Eli <efiliba@hotmail.com>
2016-03-30 14:43:09 +1100

    Fixed order of CORS middleware

--- es6server.js
+++ es6server.js
@@ -16,22 +16,22 @@ mongoose.connect(serverConfig.mongoURL, (error, connection) => {
 	}
 });
 
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Apply body Parser and server public assets and routes
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use('/', adminApi);
 app.use('/', carersApi);
 
-const allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', "*");
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-};
-app.use(allowCrossDomain);
-
 app.listen(serverConfig.port, (error) => {
 	if (!error) {
-		console.log(`BestChoiceCare API is running on port: ${serverConfig.port}!`);
+	    console.log(`BestChoiceCare API is running on port: ${serverConfig.port}!`);
     }
 });
